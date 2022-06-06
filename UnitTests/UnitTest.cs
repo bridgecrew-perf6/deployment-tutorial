cec2a196d285917f7fc1c0a856a1d89d57937b43
Hamza <h.sahin@qimia.de>
2022-06-06 10:53:39 +0300

    unit test added

--- UnitTests/UnitTest.cs
+++ UnitTests/UnitTest.cs
@@ -1,10 +1,23 @@
-﻿namespace DeploymentTutorial.UnitTests
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeploymentTutorial.UnitTests
 {
 	public class UnitTest
 	{
 		public bool IsItBig(int a)
 		{
 			return a > 1000;
+		}
+	}
+
+	[TestClass()]
+	public class UnitTestTests
+	{
+		[TestMethod()]
+		public void IsItBigTest()
+		{
+			var p = new UnitTest();
+			Assert.IsTrue(p.IsItBig(10000));
 		}
 	}
 }
